ed825b6a73e3eccb9f8954138a40f230302d3375
Sreeshvini03 <136834914+Sreeshvini03@users.noreply.github.com>
2023-12-29 21:57:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((req,res,next) => {
 //common paths
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
-app.use("/",listingRouter);
+app.use("/",userRouter);
 
 app.all("*" ,(req,res,next) =>{
     next(new ExpressError(404,"Page not found!"));
